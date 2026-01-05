9b8f6e841a633d285147f8d1e65721324ccc130c
Mxrsten <mxrsten@gmail.com>
2026-01-05 20:59:56 +0300

    1

--- projects/Обработка текстовых данных (занятие 2).py
+++ projects/Обработка текстовых данных (занятие 2).py
@@ -4,19 +4,24 @@ import re
 
 def search_database(path, request):
     with open(path, "r", encoding="utf-8-sig") as dbfile:
+
         database = dbfile.readlines()
         results = []
+        
         for entry in database:
             if re.search(request, entry, re.IGNORECASE):
                 results.append(entry.strip())
         return results
 
 
-path = input("Введите имя файла бд в формате txt (файл должен быть в той же папке что и программа):\n> ")
+def main():
+    path = input("Введите имя файла бд в формате txt (файл должен быть в той же папке что и программа):\n> ")
 
-request = input("Введите поисковый запрос (Например фамилию или номер телефона):\n> ")
+    request = input("Введите поисковый запрос (Например фамилию или номер телефона):\n> ")
 
-print("Результаты поиска:\n" + "\n".join(search_database(path, request)))
+    print("Результаты поиска:\n" + "\n".join(search_database(path, request)))
+
+main()
 
 # Пример использования:
 # Если в файле db.txt есть строки:
